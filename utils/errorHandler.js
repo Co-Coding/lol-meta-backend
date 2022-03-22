79b9940245fa27b38a72c889fb455033b8fd5d67
CArlos <cgfigueroa95@gmail.com>
2022-03-21 21:32:03 -0300

    error handler

---
+++ utils/errorHandler.js
@@ -0,0 +1,27 @@
+const errorHandler = (error) => {
+
+    if(!error.response) return internalServerErrorBuilder();
+
+    const errorParsed = JSON.parse(error.response.body)
+    
+    if(errorParsed.status.status_code === 404) return errorBuilder(errorParsed)
+    if(errorParsed.status.status_code === 403) return errorBuilder(errorParsed)
+
+}
+
+const errorBuilder = (error) => {
+    return {
+        status_code: error.status.status_code,
+        error: error.status.message
+    }
+}
+
+const internalServerErrorBuilder = () => {
+    return {
+        status_code: 500,
+        error: 'Internal Server Error'
+    }
+
+}
+
+module.exports = errorHandler
