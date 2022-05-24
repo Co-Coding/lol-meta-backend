2d024623831e4eb42873fe49cdda3813be72da80
CArlos <cgfigueroa95@gmail.com>
2022-05-24 13:30:22 -0300

    code cleaning

--- domain/dataProcces/queueProcess.js
+++ domain/dataProcces/queueProcess.js
@@ -1,9 +1,4 @@
-// const HttpService = require('../../infrastructure/http/httpService');
-
 const queueProcess = async (matches, queuesData) => {
-
-    // const httpService = new HttpService();
-    // const queuesData = await httpService.getRequest('https://static.developer.riotgames.com/docs/lol/queues.json');
 
     const queueProcessed = matches.map( match => {
         const queueData = queuesData.find( queue => queue.queueId === match.info.queueId )
