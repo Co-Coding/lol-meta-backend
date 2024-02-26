8f42633ba1bf83914c665f4c680dfffc0d072764
Carlos Figueroa <cgfigueroa95@gmail.com>
2024-02-25 23:55:03 -0300

    fix config env

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.API_KEY,
+    host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
